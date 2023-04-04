19c1b0549211b5364ca219e4e74519b914bf5233
mukminpitoyo <mpitoyo.2020@scis.smu.edu.sg>
2023-04-04 17:07:13 +0800

    added disaster recovery script

---
+++ disaster_recovery.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Ping the Singapore region endpoint to check if it's available
+if curl --head --fail --silent https://sg.example.com > /dev/null; then
+  echo "Singapore region is available"
+else
+  # Switch to the Tokyo workspace and apply the Terraform configuration
+  echo "Singapore region is down! Switching to Tokyo region..."
+  cd terraform-glam-jp-backup
+  terraform workspace select glam-jp
+  terraform apply -auto-approve
+fi
